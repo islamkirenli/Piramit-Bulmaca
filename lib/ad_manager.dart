e970a791e80fdc283a905e71183a2e1cfd3e29c5
islam kirenli <islamkirenli@islam-MacBook-Air.local>
2025-03-16 03:07:18 +0300

    Ads fixed.

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -92,9 +92,9 @@ class AdManager {
   // Platforma göre banner reklam birimi ID'si
   static String get _bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/2934735716'; // Android test ID
+      return 'ca-app-pub-3915721961755607/2708723730'; // Android test ID
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716'; // iOS test ID (gerektiğinde değiştirin)
+      return 'ca-app-pub-3915721961755607/3606501307'; // iOS test ID (gerektiğinde değiştirin)
     } else {
       throw UnsupportedError("Desteklenmeyen platform");
     }
@@ -103,9 +103,9 @@ class AdManager {
   // Platforma göre interstitial reklam birimi ID'si
   static String get _interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/4411468910'; // Android test ID
+      return 'ca-app-pub-3915721961755607/2708723730'; // Android test ID
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/4411468910'; // iOS test ID (gerektiğinde değiştirin)
+      return 'ca-app-pub-3915721961755607/4071870318'; // iOS test ID (gerektiğinde değiştirin)
     } else {
       throw UnsupportedError("Desteklenmeyen platform");
     }
@@ -114,9 +114,9 @@ class AdManager {
   // Platforma göre farklı reklam birimi ID'si döndürür
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/5224354917'; // Android için test reklam ID'si
+      return 'ca-app-pub-3915721961755607/7482904195'; // Android için test reklam ID'si
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/1712485313'; // iOS için test reklam ID'si
+      return 'ca-app-pub-3915721961755607/6978473320'; // iOS için test reklam ID'si
     } else {
       throw UnsupportedError("Desteklenmeyen platform");
     }
